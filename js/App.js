2a0dcf37287741c686fcf839a3263b65f230f5a8
dtrwoga <dtrwoga@gmail.com>
2017-08-30 07:57:24 +0200

    board.createColumn is not a function

--- js/App.js
+++ js/App.js
@@ -1,43 +1,30 @@
-﻿// OGÓLNA FUNKCJA
-
+﻿
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  'X-Client-Id': 'X-Client-Id',
-  'X-Auth-Token': 'X-Auth-Token'
+  'X-Client-Id': '1909',
+  'X-Auth-Token': '98b2049d72e227ef02f721ade72d3754'
 };
-
-
 $.ajaxSetup({
 	headers: myHeaders
 });
 
 $.ajax({
-    url: baseUrl + '/board',
-    method: 'GET',
-    success: function(response) {
-      setupColumns(response.columns);
-    }
+  url: baseUrl + '/board',
+  method: 'GET',
+  success: function(response) {
+    setupColumns(response.columns);
+  }
 });
-
-
 function setupColumns(columns) {
-    columns.forEach(function (column) {
-  		var col = new Column(column.id, column.name);
-        board.createColumn(col);
-    });
-}
-
-function setupColumns(columns) {
-    columns.forEach(function (column) {
-  		var col = new Column(column.id, column.name);
-        board.createColumn(col);
-        setupCards(col, column.cards);
-    });
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.createColumn(col);
+		setupCards(col, column.cards);
+	});
 }
 function setupCards(col, cards) {
 	cards.forEach(function (card) {
-        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-    	col.createCard(card);
-  	})
+	    var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+	  	col.createCard(card);
+  });
 }
-
